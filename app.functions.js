3cb16d743ef741880497e484c5abd59f34a60ed0
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-04-25 13:56:02 +0200

    .

--- app.functions.js
+++ app.functions.js
@@ -1,6 +1,13 @@
-const ejs      = require('ejs')
+const ejs   = require('ejs')
+const model = require('./models/frase.model')
 
 let mostrar_frase = (req, res) => {
+    const {query, params, body} = req
+    console.log('Query: ', query)
+    console.log('Params: ', params)
+    console.log('Body: ', body)
+
+
     // Renderizar la pagina de frase aleatoria
     let queries = {
         frase: 'fasfasf asf asf asf asf asf asf asf as fas fas',
@@ -26,7 +33,7 @@ let insertar_frase = (req, res) => {
     ejs.renderFile('./templates/insertar_frase.ejs', {}, {}, callback)
  }
 
- let recoge_datos = (req, res) => {
+ let recoge_datos = async (req, res) => {
     const {query, params, body} = req
     console.log('Query: ', query)
     console.log('Params: ', params)
@@ -36,10 +43,37 @@ let insertar_frase = (req, res) => {
     const author = body.author
     const frase = body.frase
 
-    // redireccionar a /insertar
+    // Guardar en la base de datos
+    let modelo = new model()
+    modelo.author = author
+    modelo.frase = frase
+    modelo = await modelo.save()
+
+    modelo.save().then((data)=>{
+        console.log('Datos guardados')
+        console.debug(data)
+
+         // redireccionar a /insertar
+        res.redirect('/insertar')
+    }).catch((error)=>{
+        console.error(error)
+    })
+
+
+}
+
+const mostrar_slug = (req, res) => {
+    const {query, params, body} = req
+    console.log('===================')
+    console.log('Query: ', query)
+    console.log('Params: ', params)
+    console.log('Body: ', body)
+    console.log('===================')
+
     res.redirect('/insertar')
 }
 
+exports.mostrar_slug   = mostrar_slug
 exports.recoge_datos   = recoge_datos
 exports.insertar_frase = insertar_frase
 exports.mostrar_frase  = mostrar_frase
--- app.routing.js
+++ app.routing.js
@@ -4,6 +4,7 @@ const router  = express.Router();
 const fn = require('./app.functions');
 
 router.get('/insertar', fn.insertar_frase)
+router.get('/mostrar/:slug', fn.mostrar_slug)
 router.get('/', fn.mostrar_frase)
 router.post('/', fn.recoge_datos)
 
